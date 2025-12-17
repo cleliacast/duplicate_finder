c5b68bb3dca313c87afbb8710e3e754a41a8e605
cleliacast <clelia.castiglia@gmail.com>
2025-12-17 15:24:01 +0100

    frist commit

--- find_duplicates.py
+++ find_duplicates.py
@@ -36,8 +36,7 @@ def get_file_size(file_path: str) -> int:
         int or None: The file size in bytes, or None if the file cannot be accessed.
     """
     # Run "pytest find_duplicates.py -k get_file_size" to test your implementation
-    raise NotImplementedError()
-
+    return os.path.getsize(file_path)
 
 def hash_first_1k_bytes(file_path: str) -> str:
     """
